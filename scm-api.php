b5e1745f4a74dc65ac4664e04992df5611f5972e
MdMbunny <info@mdmbunny.com>
2016-09-04 09:55:57 +0200

    v1.0.6

--- scm-api.php
+++ scm-api.php
@@ -3,7 +3,7 @@
  * Plugin Name:         SCM API
  * Plugin URI:          http://studiocreativo-m.it/
  * Description:         SCM Plugin Description
- * Version:             1.0.5
+ * Version:             1.0.6
  * Author:              Studio Creativo M
  * Author URI:          http://studiocreativo-m.it/
  * License:             http://www.gnu.org/licenses/gpl-3.0.html
@@ -15,62 +15,39 @@
 // *      0.0 INIT - [AUTOMATIC - DO NOT TOUCH]
 // *****************************************************
 
-    add_action( 'plugins_loaded', 'scm_plugin_init' );
+    if ( ! function_exists( 'scm_plugin_data' ) ) {
+        function scm_plugin_data( $file ) {
+            if ( ! function_exists( 'get_plugins' ) )
+                require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-    // Init Plugin
-    if ( ! function_exists( 'scm_plugin_init' ) ) {
-        function scm_plugin_init($file){
-            //print( $file . ' FROM ' . __FILE__ );
-            $file = ( $file ?: __FILE__ );
-            $plugin = scm_plugin_name( $file );
-            $slug = sanitize_title( $plugin );
-            $name = strtoupper( str_replace( '-', '_', $slug ) );
+            $plugin_folder = get_plugins( '/' . plugin_basename( dirname( $file ) ) );
+            $plugin_file = basename( ( $file ) );
+            $plugin = $plugin_folder[ $plugin_file ];
+
+            $name = $plugin[ 'Name' ];
+            $version = $plugin[ 'Version' ];
+            $slug = sanitize_title( $name );
+            $const = strtoupper( str_replace( '-', '_', $slug ) );
             $dir = dirname( $file ) . '/';
             $uri = plugin_dir_url( $file );
 
             // PLUGIN CONSTANTS
-            define( $name,                             $slug );
-            define( $name . '_VERSION',                scm_plugin_version( $file ) );
-            define( $name . '_DIR',                    $dir );
-            define( $name . '_URI',                    $uri );
-            define( $name . '_DIR_ASSETS',             $dir . 'assets/' );
-            define( $name . '_URI_ASSETS',             $uri . 'assets/' );
-            define( $name . '_DIR_LANG',               $dir . 'lang/' );
-            define( $name . '_URI_LANG',               $uri . 'lang/' );
+            define( $const,                             $slug );
+            define( $const . '_VERSION',                $version );
+            define( $const . '_DIR',                    $dir );
+            define( $const . '_URI',                    $uri );
+            define( $const . '_DIR_ASSETS',             $dir . 'assets/' );
+            define( $const . '_URI_ASSETS',             $uri . 'assets/' );
+            define( $const . '_DIR_LANG',               $dir . 'lang/' );
+            define( $const . '_URI_LANG',               $uri . 'lang/' );
 
-            // PLUGIN TEXTDOMAIN
-            load_plugin_textdomain( $slug, false, $dir . 'lang/' );
+            load_plugin_textdomain( $slug, false, $dir . 'lang/' );   
         }
-    }else{
-        //print('ALREADY EXISTS, SO...');
-        scm_plugin_init( __FILE__ );
     }
 
-    // Get Plugin Data
-    if ( ! function_exists( 'scm_plugin_data' ) ) {
-        // All Data
-        function scm_plugin_data( $file ) {
-            if ( ! function_exists( 'get_plugins' ) )
-                require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-            $plugin_folder = get_plugins( '/' . plugin_basename( dirname( $file ) ) );
-            $plugin_file = basename( ( $file ) );
-            return $plugin_folder[ $plugin_file ];
-        }
-        // Name
-        function scm_plugin_name( $file ) {
-            $plug = scm_plugin_data( $file );
-            return $plug[ 'Name' ];
-            //return scm_plugin_data( $file )[ 'Name' ];
-        }
-        // Version
-        function scm_plugin_version( $file ) {
-            $plug = scm_plugin_data( $file );
-            return $plug[ 'Version' ];
-            //return scm_plugin_data( $file )[ 'Version' ];
-        }
-    }
+    scm_plugin_data( __FILE__ ); 
+
     
-
 // ***************************************************************************************************************************************************************
 // ***************************************************************************************************************************************************************
 // *** CUSTOM CODE GOES HERE *************************************************************************************************************************************
@@ -94,7 +71,7 @@ add_action( 'scm_action_content_none', 'scm_api_redirect' );
 // *****************************************************
 
 function scm_api_redirect( $screen ){
-    if( endsWith( $screen, '/API' ) ){
+    if( $screen && endsWith( $screen, '/API' ) ){
         wp_redirect( SCM_API_URI . 'API/index.html' );
         exit;
     }
